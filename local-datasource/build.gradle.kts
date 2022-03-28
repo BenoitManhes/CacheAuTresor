5c6f212999965365f518cea35e14798b99476f5f
bmanhes-luna <benoit.manhes@lunabee.com>
2022-03-28 17:26:21 +0200

    add hilt

--- local-datasource/build.gradle.kts
+++ local-datasource/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     `android-library`
     kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 dependencies {
+    // Hilt
+    implementation(Google.dagger.hilt.android)
+    kapt(Google.dagger.hilt.compiler)
 
     implementation(project(":domain"))
     implementation(project(":common"))
