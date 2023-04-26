89ddd3007480ab86ec1cb884e3c7392f23fab824
bmanhes-luna <benoit.manhes@lunabee.com>
2023-04-26 17:37:16 +0200

    improve cache banner anim

--- app/src/main/java/com/benoitmanhes/cacheautresor/screen/home/explore/ExploreMapScreen.kt
+++ app/src/main/java/com/benoitmanhes/cacheautresor/screen/home/explore/ExploreMapScreen.kt
@@ -2,21 +2,27 @@ package com.benoitmanhes.cacheautresor.screen.home.explore
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
-import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -25,6 +31,7 @@ import com.benoitmanhes.cacheautresor.common.extensions.toModel
 import com.benoitmanhes.cacheautresor.utils.AppConstants
 import com.benoitmanhes.designsystem.molecule.button.fabbutton.FabButtonType
 import com.benoitmanhes.designsystem.molecule.button.fabiconbutton.FabIconButton
+import com.benoitmanhes.designsystem.res.Dimens
 import com.benoitmanhes.designsystem.res.icons.iconpack.Layer
 import com.benoitmanhes.designsystem.res.icons.iconpack.Position
 import com.benoitmanhes.designsystem.res.icons.iconpack.PositionCurrent
@@ -60,6 +67,12 @@ internal fun ExploreMapScreen(
     val mapViewState = rememberMapViewWithLifecycle()
     val context = LocalContext.current
     val scope = rememberCoroutineScope()
+    var lastCacheSelected: UICache? by remember {
+        mutableStateOf(null)
+    }
+    lastCacheSelected = remember(uiState.cacheSelected) {
+        uiState.cacheSelected ?: lastCacheSelected
+    }
 
     val markerFolder = remember { FolderOverlay() }
 
@@ -142,7 +155,6 @@ internal fun ExploreMapScreen(
         Column(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .animateContentSize()
                 .fillMaxWidth()
                 .wrapContentHeight()
                 .padding(CTTheme.spacing.large),
@@ -184,11 +196,28 @@ internal fun ExploreMapScreen(
                     )
                 }
             }
-            uiState.cacheSelected?.let { uiCache ->
-                CacheBanner(
-                    uiCache = uiCache,
-                    onClick = navigateToCacheDetail,
-                )
+            Box(Modifier.wrapContentHeight()) {
+                Box(modifier = Modifier.height(height = Dimens.Size.cacheCardHeight)) {}
+
+                this@Column.AnimatedVisibility(
+                    visible = uiState.cacheSelected != null,
+                    enter = slideInVertically(
+                        animationSpec = tween(),
+                        initialOffsetY = { (it * 1.5).toInt() },
+                    ),
+                    exit = slideOutVertically(
+                        animationSpec = tween(),
+                        targetOffsetY = { (it * 1.5).toInt() },
+                    )
+                ) {
+
+                    (uiState.cacheSelected ?: lastCacheSelected)?.let { uiCache ->
+                        CacheBanner(
+                            uiCache = uiCache,
+                            onClick = navigateToCacheDetail,
+                        )
+                    }
+                }
             }
         }
     }
