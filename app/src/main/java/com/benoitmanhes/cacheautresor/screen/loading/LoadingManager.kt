0933b233733f58b06f8e9b576d4e6c8090965a3d
bmanhes-luna <benoit.manhes@lunabee.com>
2023-08-23 11:59:40 +0200

    min loading time

--- app/src/main/java/com/benoitmanhes/cacheautresor/screen/loading/LoadingManager.kt
+++ app/src/main/java/com/benoitmanhes/cacheautresor/screen/loading/LoadingManager.kt
@@ -1,8 +1,14 @@
 package com.benoitmanhes.cacheautresor.screen.loading
 
+import com.benoitmanhes.domain.utils.DomainConstants
+import com.benoitmanhes.domain.utils.combineStates
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,10 +16,20 @@ import javax.inject.Singleton
 class LoadingManager @Inject constructor() {
 
     private val _isLoading = MutableStateFlow(false)
-    val isLoading: StateFlow<Boolean> get() = _isLoading.asStateFlow()
+    private val forceLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean>
+        get() = combineStates(
+            _isLoading.asStateFlow(),
+            forceLoading.asStateFlow(),
+        ) { (a, b) -> a || b }
 
     fun showLoading() {
         _isLoading.value = true
+        CoroutineScope(Dispatchers.IO).launch {
+            forceLoading.emit(true)
+            delay(DomainConstants.Loading.minLoadingDuration.inWholeMilliseconds)
+            forceLoading.emit(false)
+        }
     }
 
     fun hideLoading() {
--- domain/src/main/java/com/benoitmanhes/domain/utils/DomainConstants.kt
+++ domain/src/main/java/com/benoitmanhes/domain/utils/DomainConstants.kt
@@ -1,10 +1,16 @@
 package com.benoitmanhes.domain.utils
 
 import kotlin.math.pow
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 object DomainConstants {
     object Location {
         const val coordinatesDecimals: Int = 5
         val coordinatesApproximateError: Double = 10.0.pow(-coordinatesDecimals.toDouble())
+    }
+
+    object Loading {
+        val minLoadingDuration: Duration = 500.milliseconds
     }
 }
